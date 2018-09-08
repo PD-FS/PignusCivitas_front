57480fb556b23da7822fe22807bc3d026afc3655
Carlos Mario Toro <cmtoro0@gmail.com>
2018-09-08 15:04:57 -0500

    Se adiciona mockup securityAgents

--- src/pages/security-agents/security-agents.ts
+++ src/pages/security-agents/security-agents.ts
@@ -10,16 +10,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-security-agents',
-  templateUrl: 'security-agents.html',
+    selector: 'page-security-agents',
+    templateUrl: 'security-agents.html',
 })
 export class SecurityAgentsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    public securityAgents: {}[];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SecurityAgentsPage');
-  }
+    public communityId: number;
 
+    constructor(public navCtrl: NavController,
+        public navParams: NavParams) {}
+
+    public selectCommunity(event: any) {
+        console.log(event);
+        this.loadSecurityAgents();
+    }
+
+    private loadSecurityAgents(): void {
+        if (!this.communityId) {
+            return;
+        }
+        this.securityAgents = [
+            {
+                id: 0,
+                name: 'Elvira Vigilar',
+                role: 'Vigilante',
+                photo: 'https://lh3.googleusercontent.com/J7Mqr3sfCxZu51OfXLRXld7UWh4e75AwELsC5xJY3Ckcm9jwdItlwms5doNZfrSAkQ=s180-rw'
+            },
+            {
+                id: 1,
+                name: 'Carlos Omar Sacabandido',
+                role: 'Vigilante',
+                photo: 'https://lh3.googleusercontent.com/J7Mqr3sfCxZu51OfXLRXld7UWh4e75AwELsC5xJY3Ckcm9jwdItlwms5doNZfrSAkQ=s180-rw'
+            },
+            {
+                id: 2,
+                name: 'Enzo Villamizar',
+                role: 'Vigilante',
+                photo: 'https://lh3.googleusercontent.com/J7Mqr3sfCxZu51OfXLRXld7UWh4e75AwELsC5xJY3Ckcm9jwdItlwms5doNZfrSAkQ=s180-rw'
+            }
+        ];
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad SecurityAgentsPage');
+    }
+
+    public itemTapped(event:any, agent: any): void {
+        console.log('event: ' + event);
+        console.log('agent: ' + agent);
+    }
 }
