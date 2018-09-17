bae0947a1e3c033b67db1793bccf9bbfb719e34e
Carlos Mario Toro <cmtoro0@gmail.com>
2018-09-16 19:11:34 -0500

    Se ajusta vista Objeto Perdido Detalle

--- src/pages/lost-object-detail/lost-object-detail.ts
+++ src/pages/lost-object-detail/lost-object-detail.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the LostObjectDetailPage page.
@@ -21,13 +22,16 @@ export class LostObjectDetailPage {
 
     public object: any = null;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+    constructor(private navCtrl: NavController,
+        private navParams: NavParams,
+        private toastCtrl: ToastController) {
         const objectId = this.navParams.get('objectId');
         console.log(objectId);
         this.object = {
             name: 'Sombrilla',
             notes: 'Sombrilla marilla de éxito',
-            photo: 'https://i.pinimg.com/originals/a8/a5/d2/a8a5d2cacda2c9bfa7a961e756a1cbc4.png'
+            photo: 'https://i.pinimg.com/originals/a8/a5/d2/a8a5d2cacda2c9bfa7a961e756a1cbc4.png',
+            status: ''
         }
         this.title = this.object.name;
     }
@@ -36,8 +40,23 @@ export class LostObjectDetailPage {
         console.log('ionViewDidLoad LostObjectDetailPage');
     }
 
-    public enableEdition(){
+    public enableEdition() {
         this.readOnly = false;
+    }
+
+    public saveObject() {
+        this.readOnly = true;
+    }
+
+    public deliver() {
+        const toast = this.toastCtrl.create({
+            message: 'Confirmado!',
+            duration: 3000,
+            position: 'top',
+            showCloseButton: true,
+            closeButtonText: 'x'
+        });
+        toast.present();
     }
 
 }
