6824afece7360f13b26f0dbad07156644aa991c9
Gustavo Maggi <gmaggiw@gmail.com>
2018-09-25 23:39:06 -0500

    Citofono funcional

--- src/pages/intercom/intercom.ts
+++ src/pages/intercom/intercom.ts
@@ -1,6 +1,8 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import Peer from 'peerjs';
+import { fromEvent } from 'rxjs';
+
+declare var Peer: any;
 
 /**
  * Generated class for the IntercomPage page.
@@ -14,60 +16,114 @@ import Peer from 'peerjs';
   selector: 'page-intercom',
   templateUrl: 'intercom.html',
 })
-export class IntercomPage implements OnInit{
+export class IntercomPage implements AfterViewInit {
 
-  @ViewChild('mensaje') mensaje : ElementRef;
-  @ViewChild('video') video : ElementRef;
+  @ViewChild('myvideo') myvideo: ElementRef;
+  @ViewChild('myanswerButton') myanswerButton: ElementRef;
+  @ViewChild('myhangup') myhangup: ElementRef;
 
-  private peer : Peer;
-  private peerToConnect: string;
-  private callOpen: boolean = true;
+
+  peer;
+  mypeerid;
+  anotherid;
+  mymsg: string;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams) {
 
+    // PeerServer Configuration
+    const peerserver = {
+      host: 'rtc.pignus.app',
+      port: 443,
+      path: '/',
+      secure: true,
+      debug: 3,
+    };
 
-
-    this.peer = new Peer({key: 'pignuscivitasapp'})
+    this.peer = new Peer(null, peerserver);
 
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
 
-    let m = this.mensaje.nativeElement
+    // Get Assigned Peer id
+    this.peer.on('open', id => {
+      this.mypeerid = id;
+    });
 
-    this.peer.on('open', function () {
-        m.innerHTML = "Esperando conexión ...";
+    // Variables and constants
+    const n = <any>navigator;
+    const video = this.myvideo.nativeElement;
+    const answerButton = this.myanswerButton.nativeElement;
+    const hangupButton = this.myhangup.nativeElement;
+    hangupButton.style = 'display: none';
+    answerButton.style = 'display: none';
+
+    this.mymsg = 'Awaiting...';
+
+    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
+
+    const answerOnClick = fromEvent(answerButton, 'click');
+    const hangupOnClick = fromEvent(hangupButton, 'click');
+
+    // Answer call context
+    this.peer.on('call', call => {
+      //if (n.mediaDevices) {
+        n.getUserMedia({video: true, audio: true}, stream => {
+          answerButton.style = 'display: inline';
+          this.mymsg = 'Incoming Call...';
+
+          // Answer click event handler
+          answerOnClick.subscribe(x => {
+            call.answer(stream);
+            call.on('stream', remotestream => {
+              video.src = URL.createObjectURL(remotestream);
+              video.play();
+              answerButton.style = 'display: none';
+              hangupButton.style = 'display: inline';
+              this.mymsg = 'Connected';
+            });
+          }, err => {
+            console.log('Error:', err);
+          }, () => {
+            console.log('Completed');
+          });
+
+          // Hangup click event handler
+          hangupOnClick.subscribe(x => {
+            call.close();
+          }, err => {
+            console.log('Error:', err);
+          }, () => {
+            console.log('Completed');
+          });
+
+          call.on('close', () => {
+            video.src = '';
+            hangupButton.style = 'display: none';
+            this.mymsg = 'Awaiting...';
+            stream.getAudioTracks()[0].stop();
+            stream.getVideoTracks()[0].stop();
+          });
+
+
+       }, error => {
+          this.mymsg = 'Failed to get local stream ' + error;
+        });
+      //}
 
     });
 
-    let v = this.video.nativeElement
+    // On Error Handler
+    this.peer.on('error', err => {
+      this.mymsg = err;
+    });
 
-    var n = <any>navigator
-    n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
-    let callStatus = this.callOpen
-    this.peer.on('call', function(call) {
-
-      n.getUserMedia({video: true, audio: true}, function(stream) {
-        if(callStatus){
-          call.answer(stream); // Answer the call with an A/V stream.
-        } else {
-          call.close()
-        }
-        call.on('stream', function(remoteStream) {
-          m.innerHTML = "Conectado"
-          v.src = URL.createObjectURL(remoteStream)
-          v.play()
-        });
-
-        call.on('close', function(){
-          m.innerHTML = "Finalizado"
-          v.pause()
-        });
-
-      }, function(err) {
-        console.log('Failed to get local stream' ,err);
-      });
+    // On disconnected handler
+    this.peer.on('disconnected', () => {
+      console.log('Reconecting');
+      this.peer.reconnect();
+      console.log();
     });
 
   }
@@ -77,35 +133,47 @@ export class IntercomPage implements OnInit{
   }
 
 
-  disconnectCall()
-  {
-    this.callOpen = false
-    let m = this.mensaje.nativeElement;
-    this.peer.on('call', function(call) {
-      call.close()
-      console.log('entre en disconect')
-      m.innerHTML = "Llamada finalizada"
-    })
-  }
-
   makeCall() {
 
-    let v = this.video.nativeElement
-    let peer = this.peer
-    let connid = this.peerToConnect
-    let m = this.mensaje.nativeElement
-
-    var n = <any>navigator
+    // Variables and constants
+    const n = <any>navigator;
+    const video = this.myvideo.nativeElement;
+    const hangupButton = this.myhangup.nativeElement;
+    this.mymsg = 'Calling...';
     n.getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
-    n.getUserMedia({video: false, audio: true}, function(stream) {
-      var call = peer.call(connid, stream);
-      call.on('stream', function(remoteStream) {
-        m.innerHTML = "Conectado"
-        v.src = URL.createObjectURL(remoteStream)
-        v.play()
+
+    const hangupOnClick = fromEvent(hangupButton, 'click');
+
+    // Making the Call
+    n.getUserMedia({video: true, audio: true}, (stream) => {
+      const call = this.peer.call(this.anotherid, stream);
+
+      call.on('close', () => {
+        video.src = '';
+        hangupButton.style = 'display: none';
+        this.mymsg = 'Awaiting...';
+        stream.getAudioTracks()[0].stop();
+        stream.getVideoTracks()[0].stop();
       });
-    }, function(err) {
-      console.log('Failed to get local stream' ,err);
+
+      call.on('stream', remotestream => {
+        video.src = URL.createObjectURL(remotestream);
+        video.play();
+        hangupButton.style = 'display:inline';
+        this.mymsg = 'Connected';
+
+        // Hangup click event handler
+        hangupOnClick.subscribe(x => {
+          call.close();
+        }, err => {
+          console.log('Error:', err);
+        }, () => {
+          console.log('Completed');
+        });
+      });
+    }, error => {
+      console.log('Failed to get local stream ' + error);
     });
+
   }
 }
