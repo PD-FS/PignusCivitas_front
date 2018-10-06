ed5dbee1facc5d70fc90864ef1af9f6e1f530996
jhonjimtovar <jhon.jimenez.tovar@outlook.com>
2018-10-06 12:12:55 -0500

    Agregar vehículo: ajustar el adjuntar

--- src/pages/add-vehicle/add-vehicle.ts
+++ src/pages/add-vehicle/add-vehicle.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ImagePicker } from '@ionic-native/image-picker';
 
 /**
  * Generated class for the AddVehiclePage page.
@@ -35,6 +36,7 @@ photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEm0yZS9VwA7f8Zjp7
 
 constructor(public navCtrl: NavController,
 public navParams: NavParams,
+private imagePicker: ImagePicker,
 private formBuilder: FormBuilder) {
 
 }
@@ -43,9 +45,6 @@ ionViewDidLoad() {
 console.log('ionViewDidLoad AddVehiclePage');
 }
 
-public attachImage() {
-
-}
 
 public save() {
 this.saved = true;
@@ -53,6 +52,20 @@ console.log(this.formulario.status);
 if (this.formulario.valid) {
 this.navCtrl.pop();
 }
+}
+
+public attachImage(): void {
+  const options = {
+      maximumImagesCount: 1,
+      width: 800,
+      height: 800,
+      quality: 80
+  };
+  this.imagePicker.getPictures(options).then((results) => {
+      for (var i = 0; i < results.length; i++) {
+          console.log('Image URI: ' + results[i]);
+      }
+  }, (err) => { });
 }
 
 }
