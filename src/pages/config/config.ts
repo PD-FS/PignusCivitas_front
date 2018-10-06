dd9fd512d601e03e6026522eda7f8cb60cecbc4c
jhonjimtovar <jhon.jimenez.tovar@outlook.com>
2018-10-06 15:52:10 -0500

    Configurar: agregar icono de editar y ajuste de listas

--- src/pages/config/config.ts
+++ src/pages/config/config.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SecurityProvider } from '../../providers/security/security';
+import { ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the ConfigPage page.
@@ -16,10 +17,17 @@ import { SecurityProvider } from '../../providers/security/security';
 })
 export class ConfigPage {
 
-  userData: {name: string, code: string, email: string, community: string, img: string};
+  userData: {name: string, code: string, email: string, community: string, img: string, address: string};
+
+  public showButton: boolean = false;
+
+  public showEditButton: boolean = true;
+
+  public readOnly: boolean = true;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
+              private toastCtrl: ToastController,
               private security: SecurityProvider) {
 
       this.security.getNotificator().subscribe(
@@ -30,6 +38,7 @@ export class ConfigPage {
                                code: 'CIA87632',
                                email: 'elviravigilar@tusegurida.com',
                                community: 'Conjunto Las Aguas',
+                               address: 'Calle 25 # 45 - 43',
                                img: 'assets/imgs/vigilante.png'}
             }
             else if(data == 1)
@@ -38,12 +47,14 @@ export class ConfigPage {
                                code: '',
                                email: 'enzocorleone@gmail.com',
                                community: '',
+                               address: 'Calle 25 # 45 - 43',
                                img: 'assets/imgs/ciudadano.png'}
             } else{
               this.userData = {name: '',
                                code: '',
                                email: '',
                                community: '',
+                               address: '',
                                img: ''}
             }
         }
@@ -53,5 +64,25 @@ export class ConfigPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad ConfigPage');
   }
+  public enableEdition() {
+    this.readOnly = false;
+    this.showButton = true;
+    this.showEditButton = false;
+}
+
+public saveObject() {
+  this.readOnly = true;
+  this.showButton = false;
+  this.showEditButton = true;
+  const toast = this.toastCtrl.create({
+    message: 'Guardado exitoso!',
+    duration: 3000,
+    position: 'top',
+    showCloseButton: true,
+    closeButtonText: 'x'
+});
+toast.present();
+
+}
 
 }
