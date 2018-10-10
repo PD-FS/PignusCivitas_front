1056eb8c8bac22b52c15b3acfde2766fb2af7fe7
Gustavo Maggi <gmaggiw@gmail.com>
2018-10-10 14:56:29 -0500

    Se elimina configuracion de firebase

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCa6yBHtGqQgrfuq_bH7wXDaa3K006tvwo",
-  authDomain: "pignuscivitas.firebaseapp.com",
-  databaseURL: "https://pignuscivitas.firebaseio.com",
-  projectId: "pignuscivitas",
-  storageBucket: "pignuscivitas.appspot.com",
-  messagingSenderId: "366480062828"
-}
